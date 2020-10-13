72baacae7dfe2461ebad6606aa63b0bc6534fa76
rrih <rsklv@icloud.com>
2020-10-14 00:36:22 +0900

    タイトル修正

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -34,8 +34,7 @@ $cakeDescription = 'お気持ち.com';
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <title>
-        <?= $cakeDescription ?>:
-        <?= $this->fetch('title') ?>
+        <?= $cakeDescription ?>
     </title>
     <?= $this->Html->meta('icon') ?>
 
