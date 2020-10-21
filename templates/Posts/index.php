07dc58bde4bb3f4ca1454039f8810232899d9ada
rrih <rsklv@icloud.com>
2020-10-22 04:10:31 +0900

    リアクションと日時の表示配置変更

--- templates/Posts/index.php
+++ templates/Posts/index.php
@@ -10,44 +10,44 @@
             <?php foreach ($posts as $post): ?>
                 <!-- post card -->
                 <div class="card my-3 shadow-sm">
-                    <div class="card-header h5 border-bottom"><?= $post->user_name ? $post->user_name : 'null' ?></div>
-                    <div class="card-body">
+                    <div class="card-header h5 border-bottom text-left"><?= $post->user_name ? $post->user_name : 'null' ?></div>
+                    <div class="card-body text-left">
                         <?= h($post->text) ?>
                     </div>
-                    <div class="card-text" style="font-size: 30px">
-                        <!-- TODO モーダル開いて、コメントを投稿できるようにする -->
-                        <a href="#" class="btn btn-outline-primary rounded-pill">
-                            <i class="far fa-comment"></i>コメント
-                        </a>
-                        <a href="#" class="btn btn-outline-dark rounded-pill">
-                        <i class="fas fa-retweet"></i>拡散
-                        </a>
-                        <?= $this->Form->postLink(
-                            __('👍 ' . $post->like_count),
-                            [
-                                'action' => 'plusLikeCount',
-                                $post->id
-                            ],
-                            [
-                                'class' => 'btn btn-outline-success rounded-pill'
-                            ]
-                        )
-                        ?>
-                        <?= $this->Form->postLink(
-                            __('👎 ' . $post->dislike_count),
-                            [
-                                'action' => 'plusDislikeCount',
-                                $post->id
-                            ],
-                            [
-                                'class' => 'btn btn-outline-danger rounded-pill'
-                            ]
-                        )
-                        ?>
+                    <div class="card-text text-right text-muted">
+                        表明日時 <?= $post->post_created->format('yy-m-d h:m:s') ?>
                     </div>
                     <?php if ($post->post_created) { ?>
-                        <div class="card-footer text-muted">
-                            投稿日時 <?= $post->post_created->format('yy-m-d h:m:s') ?>
+                        <div class="card-footer text-muted text-right" style="font-size: 30px">
+                            <!-- TODO モーダル開いて、コメントを投稿できるようにする -->
+                            <a href="#" class="btn btn-outline-primary rounded-pill">
+                                <i class="far fa-comment"></i>コメント
+                            </a>
+                            <a href="#" class="btn btn-outline-dark rounded-pill">
+                            <i class="fas fa-retweet"></i>拡散
+                            </a>
+                            <?= $this->Form->postLink(
+                                __('👍 ' . $post->like_count),
+                                [
+                                    'action' => 'plusLikeCount',
+                                    $post->id
+                                ],
+                                [
+                                    'class' => 'btn btn-outline-success rounded-pill'
+                                ]
+                            )
+                            ?>
+                            <?= $this->Form->postLink(
+                                __('👎 ' . $post->dislike_count),
+                                [
+                                    'action' => 'plusDislikeCount',
+                                    $post->id
+                                ],
+                                [
+                                    'class' => 'btn btn-outline-danger rounded-pill'
+                                ]
+                            )
+                            ?>
                         </div>
                     <?php } ?>
                 </div>
