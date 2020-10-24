4661df6649fc981b12a106f88dc5064b3a865baf
rrih <rsklv@icloud.com>
2020-10-25 07:27:24 +0900

    不要箇所削除

---
+++ templates/layout/messages/view.php
@@ -0,0 +1,24 @@
+<div class="text-center container">
+    <div>
+        <?= $otherUser->name ?>
+        <?= $this->Form->create(null, ['url' => '/messages/view/' . $otherUser->id, 'type' => 'post']) ?>
+            <?php
+                echo $this->Form->control('message', ['label' => 'メッセージ', 'type' => 'textarea', 'class' => 'form-control', 'empty' => false, 'required' => true, 'name' => 'message', 'maxlength' => 500]);
+            ?>
+            <div class="text-center my-3"><?= $this->Form->button(__('送信'), ['class' => 'btn btn-outline-primary rounded-pill px-4 py-3']) ?></div>
+        <?= $this->Form->end() ?>
+        <?php foreach ($messages as $msg): ?>
+            <div class="border">
+                <div>
+                    送信者 <?= $this->Users->getOneUserName($msg->message_user_id) ?>
+                </div>
+                <div>
+                    <?= $msg->message ?>
+                </div>
+                <div>
+                    <?= $msg->created->format('yy-m-d h:m:s') ?>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
