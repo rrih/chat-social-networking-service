b66019f4055724cfec3d2e806d81b0f65b776c96
rrih <rsklv@icloud.com>
2020-10-25 12:10:01 +0900

    余計なラベル削除

--- templates/Messages/view.php
+++ templates/Messages/view.php
@@ -5,7 +5,7 @@
         </div>
         <?= $this->Form->create(null, ['url' => '/messages/view/' . $otherUser->id, 'type' => 'post']) ?>
             <?php
-                echo $this->Form->control('message', ['label' => 'メッセージ', 'type' => 'textarea', 'class' => 'form-control', 'empty' => false, 'required' => true, 'name' => 'message', 'maxlength' => 500]);
+                echo $this->Form->control('message', ['type' => 'textarea', 'class' => 'form-control', 'empty' => false, 'required' => true, 'name' => 'message', 'maxlength' => 500]);
             ?>
             <div class="text-center my-3"><?= $this->Form->button(__('送信'), ['class' => 'btn btn-outline-primary rounded-pill px-4 py-3']) ?></div>
         <?= $this->Form->end() ?>
