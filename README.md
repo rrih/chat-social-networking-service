6aa846a8e6c421e71ecaddeaad28823a00601354
rrih <rsklv@icloud.com>
2020-11-21 18:58:54 +0900

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # feel
-お気持ち表明の場
+web app
 
 ## 本番環境
 https://feel-prod.herokuapp.com
---
+++ templates/Messages/index.php
@@ -0,0 +1,27 @@
+<div class="text-center container">
+    <div>
+        <div>
+            メッセージ
+            <?= $this->Users->getOneUserName($otherUser->id) ?>とのメッセージ
+        </div>
+        <?= $this->Form->create(null, ['url' => '/messages/view/' . $otherUser->id, 'type' => 'post']) ?>
+            <?php
+                echo $this->Form->control('message', ['label' => false, 'type' => 'textarea', 'class' => 'form-control', 'empty' => false, 'required' => true, 'name' => 'message', 'maxlength' => 500]);
+            ?>
+            <div class="text-center my-3"><?= $this->Form->button(__('送信'), ['class' => 'btn rounded-pill px-4 py-3']) ?></div>
+        <?= $this->Form->end() ?>
+        <?php foreach ($messages as $msg): ?>
+            <div class="card my-3 shadow-sm">
+                <div class="card-header border-bottom">
+                    <div><?= $this->Users->getOneUserName($msg->message_user_id) ?></div>
+                </div>
+                <div class="card-body text-left">
+                    <?= $msg->message ?>
+                </div>
+                <div class="card-footer" style="font-size: 15px">
+                    <?= $msg->created->format('yy-m-d h:m:s') ?>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
